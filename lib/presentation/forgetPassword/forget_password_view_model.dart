47398ac428be9ce96a68b8bb30539b8b8c16f1e6
abdghaniallag <abdghaniallag@gmail.com>
2022-01-18 00:57:22 +0100

    Forget password implimentation

---
+++ lib/presentation/forgetPassword/forget_password_view_model.dart
@@ -0,0 +1,16 @@
+import 'package:mvvm_first_c/presentation/base/base.dart';
+
+class ForgetPasswordViewModel extends BaseViewModel{
+
+  @override
+  void start() {
+    // TODO: implement start 
+  }
+  
+}
+abstract class ForgetPasswordViewModelInputs {
+  
+}
+abstract class ForgetPasswordViewModelOutputs {
+  
+}
