6335616c98f99c4e188005a9a431b1b3570658b9
abdghaniallag <abdghaniallag@gmail.com>
2022-01-27 23:39:24 +0100

    adding stream controller to forgot password view

--- lib/presentation/forgetPassword/forgetPassword.dart
+++ lib/presentation/forgetPassword/forgetPassword.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
+
+import 'forget_password_view_model.dart';
 class ForgetPasswordView extends StatefulWidget {
   const ForgetPasswordView({ Key? key }) : super(key: key);
 
@@ -7,10 +11,29 @@ class ForgetPasswordView extends StatefulWidget {
 }
 
 class _ForgetPasswordViewState extends State<ForgetPasswordView> {
+
+  ForgetPasswordViewModel _viewModel=ForgetPasswordViewModel();
+TextEditingController _emailController=TextEditingController();
+  void _bind() {
+    _viewModel.start();
+    _emailController.addListener(() =>_viewModel.setEmaill(_emailController.text)); 
+  }
+@override
+void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       
     );
   }
+  @override 
+  void dispose() { 
+    _viewModel.dispose();
+    super.dispose();
+  }
+
 }
--- lib/presentation/forgetPassword/forget_password_view_model.dart
+++ lib/presentation/forgetPassword/forget_password_view_model.dart
@@ -1,16 +1,50 @@
+import 'dart:async';
+ 
 import 'package:mvvm_first_c/presentation/base/base.dart';
 
-class ForgetPasswordViewModel extends BaseViewModel{
+class ForgetPasswordViewModel extends BaseViewModel
+    with ForgetPasswordViewModelInputs, ForgetPasswordViewModelOutputs {
+  StreamController _emailStreamController =
+      StreamController<String>.broadcast();
+  @override
+  void dispose() {
+    _emailStreamController.close();
+  }
 
   @override
   void start() {
-    // TODO: implement start 
+    // TODO: implement start
   }
-  
+
+  @override 
+  Sink get InputEmail => _emailStreamController.sink;
+
+  @override 
+  Stream<bool> get outputIsEmailValid =>
+      _emailStreamController.stream.map((email) => isEmailValid(email));
+  bool isEmailValid(String email) {
+    return email.isNotEmpty;
+  }
+
+  @override
+  resetPassword() {
+    // TODO: implement resetPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  setEmaill(String email) { 
+    InputEmail.add(email);
+  }
 }
+
 abstract class ForgetPasswordViewModelInputs {
-  
+  setEmaill(String email);
+  resetPassword();
+
+  Sink get InputEmail;
 }
+
 abstract class ForgetPasswordViewModelOutputs {
-  
+  Stream<bool> get outputIsEmailValid;
 }
